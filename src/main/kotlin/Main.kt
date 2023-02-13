5183a7e1f3e578e2177c1893c287004ec5c52d8b
marisahleleshep <leleshepmmarisaa@gmail.com>
2023-02-13 14:15:05 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    var fullName :String= "Marrion Mideva"
+    println(fullName)
+    var weightinkg : Byte= 60
+    println(weightinkg)
+    var phoneNumber :String= "0723454376"
+    println(phoneNumber)
+    var citizen : Boolean= true
+    println(citizen)
+
+
+
+
+}
